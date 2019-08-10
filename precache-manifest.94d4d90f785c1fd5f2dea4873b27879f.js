a6aeca6c50746be7e218d2229a8ac5b396ffda9d
Souhaib Ben Farhat <s.ben-farhat@klarx.com>
2019-08-11 01:05:48 +0200

    improvements related to  layout and content

---
+++ precache-manifest.94d4d90f785c1fd5f2dea4873b27879f.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45fe18ba96ef7898f19dbbe4a7d7fae1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c605799669d1cd09ed4f",
+    "url": "/static/css/main.9063251c.chunk.css"
+  },
+  {
+    "revision": "00ab16792061e3241288",
+    "url": "/static/js/2.88fff171.chunk.js"
+  },
+  {
+    "revision": "97a2f1654456c8d56d3a",
+    "url": "/static/js/3.eefec95c.chunk.js"
+  },
+  {
+    "revision": "cb9525912b5ba5119163",
+    "url": "/static/js/4.8f75e97e.chunk.js"
+  },
+  {
+    "revision": "178430aa018eb1243590",
+    "url": "/static/js/5.d4ad0c90.chunk.js"
+  },
+  {
+    "revision": "99063719181cb576018b",
+    "url": "/static/js/6.31e9736d.chunk.js"
+  },
+  {
+    "revision": "c605799669d1cd09ed4f",
+    "url": "/static/js/main.f2ee2d7d.chunk.js"
+  },
+  {
+    "revision": "e5b51c28e953d673235d",
+    "url": "/static/js/runtime~main.b4dc62de.js"
+  },
+  {
+    "revision": "58f3c4158fd61b02de69b9726b21d043",
+    "url": "/static/media/FiraSansCondensed-Bold.58f3c415.woff2"
+  },
+  {
+    "revision": "950de12761c4ecd000828dd642dff1fc",
+    "url": "/static/media/FiraSansCondensed-Bold.950de127.woff"
+  },
+  {
+    "revision": "9eae78824d9f614291035dc4e1fde684",
+    "url": "/static/media/FiraSansCondensed-Regular.9eae7882.woff"
+  },
+  {
+    "revision": "c861321419e30ae11ee1bc0e0d2e7abc",
+    "url": "/static/media/FiraSansCondensed-Regular.c8613214.woff2"
+  },
+  {
+    "revision": "15348d64e4371a7dd7bf889be67a614c",
+    "url": "/static/media/GloriaHallelujah.15348d64.woff"
+  },
+  {
+    "revision": "f8e5f997dde539806338bc74696fbcbd",
+    "url": "/static/media/GloriaHallelujah.f8e5f997.woff2"
+  },
+  {
+    "revision": "39aed30e7bf83d05fc7f8692531f2c71",
+    "url": "/static/media/TTMussels-Bold.39aed30e.woff"
+  },
+  {
+    "revision": "ed66843b26738118f5705643d96a1e3a",
+    "url": "/static/media/TTMussels-Bold.ed66843b.woff2"
+  },
+  {
+    "revision": "7038cdb531acc89143fe4909e4ab34c5",
+    "url": "/static/media/TTMussels-Regular.7038cdb5.woff2"
+  },
+  {
+    "revision": "e8d715d6a609d037bfc958e426731dbf",
+    "url": "/static/media/TTMussels-Regular.e8d715d6.woff"
+  },
+  {
+    "revision": "c3522cc263743d7b2b1c2861213a179a",
+    "url": "/static/media/edash-thumbnail.c3522cc2.png"
+  },
+  {
+    "revision": "7662731db68b105f2ea7dbb360415ebf",
+    "url": "/static/media/gaimz-thumbnail.7662731d.jpg"
+  },
+  {
+    "revision": "82cb8caa48076fdb719a02b7cba15e8b",
+    "url": "/static/media/header-bg.82cb8caa.jpg"
+  },
+  {
+    "revision": "7fbd57bb6ceed13fdc1fc036cb45adca",
+    "url": "/static/media/karhabti-android.7fbd57bb.png"
+  },
+  {
+    "revision": "9c0bf0819be02a14205ebc7d4a6632d5",
+    "url": "/static/media/karhabti-ios.9c0bf081.jpg"
+  },
+  {
+    "revision": "c48fee25d6eeca52b2eab2ea2047321d",
+    "url": "/static/media/rep-thumbnail.c48fee25.png"
+  },
+  {
+    "revision": "f0e2b039a813c7a25911b4dc21b512f5",
+    "url": "/static/media/souhaib.f0e2b039.jpg"
+  },
+  {
+    "revision": "5c83a334c0b7460a231131606791f846",
+    "url": "/static/media/techstamp-thumbnail.5c83a334.jpg"
+  }
+]);
